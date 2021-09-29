79243c0eb0124a91f77bf0b859fd8fe5428a88f9
wantj1 <wantj1@chinatelecom.cn>
2021-09-29 15:44:33 +0800

    增加自定义指令options，help

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+//shebang：在当前的电脑环境里找node可执行文件，之后会根据node可执行文件执行当前文件
+const program = require("commander")
+
+
+const helpOptions = require("./lib/core/help");
+//查看版本号
+program.version(require('./package.json').version) //加载package.json，读取显示版本号
+
+
+// 帮助和可选信息
+helpOptions();
+
+program.parse(process.argv) // 解析参数，放在最后
