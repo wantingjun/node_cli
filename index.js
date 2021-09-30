e69ff91835649f1891522c19f24c5208fbbd448d
wantj1 <wantj1@chinatelecom.cn>
2021-09-30 09:16:10 +0800

    创建create prject命令

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const program = require("commander")
 
 
 const helpOptions = require("./lib/core/help");
+const createCommands = require("./lib/core/create")
+
 //查看版本号
 program.version(require('./package.json').version) //加载package.json，读取显示版本号
 
 
 // 帮助和可选信息
 helpOptions();
+createCommands()
 
 program.parse(process.argv) // 解析参数，放在最后
